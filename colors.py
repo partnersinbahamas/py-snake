2bfbd353b79417d6c9343bc9154f87233ca07d6e
Denys Bokov <denys.bokov@jonasundderwolf.de>
2024-12-03 19:52:57 +0100

    feat: create colors file

---
+++ colors.py
@@ -0,0 +1,8 @@
+from named import Color
+
+# Colors
+BACKGROUND = Color(r=1, g=22, b=39)
+SNAKE = Color(r=94, g=165, b=255)
+FOOD = Color(r=109, g=197, b=53)
+WHITE = Color(r=255, g=255, b=255)
+TEXT = Color(r=255, g=253, b=65)
